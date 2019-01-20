9c793af9cf4f9f2a376d7624de926c7df0faefea
Alex Bausk <bauskas@gmail.com>
2019-01-20 23:00:00 +0200

    Bugfix for credentials

--- backend/gdrive.py
+++ backend/gdrive.py
@@ -1,7 +1,7 @@
+import os
+import json
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-import os
-
 
 
 scope = ['https://spreadsheets.google.com/feeds']
@@ -9,11 +9,11 @@ url = 'https://docs.google.com/spreadsheets/d/1gjzWEyK36pd5oY6q_oQYvv15yTFc4eGUq
 
 creds = None
 try:
-    creds = ServiceAccountCredentials.from_json_keyfile_name(
-        './credentials/googlekey.json', scope)
-except:
-    json_data = os.environ['GSPREAD_CREDENTIALS']
-    creds = ServiceAccountCredentials.from_json(json_data)
+    creds = ServiceAccountCredentials.from_json_keyfile_name('./credentials/googlekey.json', scope)
+except FileNotFoundError:
+    json_data = json.loads(os.environ['GSPREAD_CREDENTIALS'])
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(json_data, scope)
+
 
 def get_data():
     gdrive_client = gspread.authorize(creds)
