87cfce53a9aeb946ffd02c39cfd2f5e45502b8e9
Alex Bausk <bauskas@gmail.com>
2019-02-02 14:22:56 +0200

    Redis config writer

--- scripts/generate-ebs.py
+++ scripts/generate-ebs.py
@@ -7,8 +7,12 @@ try:
     redis_node_url = data.get('CacheClusters')[0].get('CacheNodes')[0].get('Endpoint').get('Address')
     redis_node_port = data.get('CacheClusters')[0].get('CacheNodes')[0].get('Endpoint').get('Port')
 
-    redis_config_template = """
-    """
+    redis_config_template = """option_settings:
+  - option_name: REDIS_HOST
+    value: {}:{}"""
+    redis_config = redis_config_template.format(redis_node_url, redis_node_port)
+    with open('./.ebextensions/secrets.config', 'w') as configfile:
+        configfile.write(redis_config)
 except Exception as e:
     pass
 
